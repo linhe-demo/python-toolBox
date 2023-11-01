80db091fb41c810cf5079a25f89cccac90981a56
muhe <muhe@kerrylan.com>
2023-11-01 11:03:39 +0800

    优化代码

--- pkg/tms.py
+++ pkg/tms.py
@@ -1,10 +1,21 @@
+import json
+
 from tools.curl import Curl
 
 
 class Tms:
-    def __init__(self, url=None, param=None):
+    def __init__(self, url1=None, url2=None, param=None):
         self.param = param
-        self.url = url
+        self.url1 = url1
+        self.url2 = url2
 
     def createOutboundOrder(self):
-        return Curl(url=self.url, param=self.param).getPostmanPostRes()
+        res = Curl(url=self.url1, param=self.param).getPostmanPostRes()
+        res = json.loads(res)
+        if res.get('code', 0) == 20000 and res.get('message', '') == 'SUCCESS':
+            data = res.get('data')
+            if len(data.get('wms_order_sn', '')) == 0:
+                return res
+            else:
+                return Curl(url=self.url2, param=json.dumps({"last_mile_order_sn": data.get("wms_order_sn")})).getPostmanPostRes()
+
--- script/tms/createOrder.py
+++ script/tms/createOrder.py
@@ -4,14 +4,9 @@ from data.tmsOrderParam import TmsOrderParam
 from pkg.tms import Tms
 from tools.file import File
 
-
 if __name__ == "__main__":
     config = File(path="\config\config.json").read_json_config()
     param = TmsOrderParam(index="outbound").getParam()
-    res = Tms(url=config["tmsUrl"]["createOutbound"], param=param).createOutboundOrder()  # 向proxy 发起创建出库单
+    res = Tms(url1=config["tmsUrl"]["createOutboundTms"], url2=config["tmsUrl"]["createOutboundWms"],
+              param=param).createOutboundOrder()  # 向proxy 发起创建出库单
     print(json.loads(res))
-
-
-
-
-
